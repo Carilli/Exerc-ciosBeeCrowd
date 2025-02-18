a480cfc7828e2d6c1d0e1676adb2d5a538894e93
João Victor M Carilli <jvcarilli@gmail.com>
2025-02-18 11:20:42 -0300

    Add files via upload

---
+++ 1008.php
@@ -0,0 +1,10 @@
+<?php
+
+$hour = readline("Digite quantas horas trabalhadas: ");
+$sal = readline("Digite o seu salário: ");
+
+$SalPorHour = $sal/$hour;
+
+echo ("O valor da sua hora é: $SalPorHour");
+
+?>
